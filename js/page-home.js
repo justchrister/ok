76345f3f686ca921fd63f53343db3567d833d2f3
christerwaters <42318403+christerwaters@users.noreply.github.com>
2020-07-01 20:24:03 +0200

    1

--- js/page-home.js
+++ js/page-home.js
@@ -1,16 +1,3 @@
-var timeoutid = 0;
-$(document).on("mousemove", function (e) {
-  var xPos = e.pageX;
-  var yPos = e.pageY;
-  // console.log(xPos, yPos);
-  $("span").css({
-    top: yPos,
-    left: xPos - 50
-  });
-
-  $("span").toggleClass("go");
-});
-
 $('#projects nav').randomize('article');
 $('article').randomize('figure');
 $('article').randomize('figure');
