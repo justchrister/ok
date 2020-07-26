75cf5e6790bce16d73db969409e9354d9df8e96b
christerwaters <42318403+christerwaters@users.noreply.github.com>
2020-07-26 23:05:13 +0200

    flipperffixxxx

--- public_html/js/partial-flipper.js
+++ public_html/js/partial-flipper.js
@@ -1,18 +1,19 @@
 const flipper = [
-  "media/project-1/01.JPG",
-  "media/project-1/02.JPG",
-  "media/project-1/03.JPG",
-  "media/project-1/04.JPG",
-  "media/project-1/05.JPG",
-  "media/project-2/01.JPG",
-  "media/project-2/02.JPG",
-  "media/project-2/03.JPG",
-  "media/project-2/04.JPG",
-  "media/project-3/01.JPG",
-  "media/project-3/02.JPG",
-  "media/project-3/03.JPG",
-  "media/project-3/04.JPG",
-  "media/project-3/05.JPG"
+  "https://cdn.kalt.co/citrusstudio.no/sykle-uten-hender/01.jpg",
+  "https://cdn.kalt.co/citrusstudio.no/sykle-uten-hender/02.jpg",
+  "https://cdn.kalt.co/citrusstudio.no/sykle-uten-hender/03.jpg",
+  "https://cdn.kalt.co/citrusstudio.no/sykle-uten-hender/04.jpg",
+  "https://cdn.kalt.co/citrusstudio.no/sykle-uten-hender/05.jpg",
+  "https://cdn.kalt.co/citrusstudio.no/for-the-bright-days/01.jpg",
+  "https://cdn.kalt.co/citrusstudio.no/for-the-bright-days/02.jpg",
+  "https://cdn.kalt.co/citrusstudio.no/for-the-bright-days/03.jpg",
+  "https://cdn.kalt.co/citrusstudio.no/for-the-bright-days/04.jpg",
+  "https://cdn.kalt.co/citrusstudio.no/for-the-bright-days/05.jpg",
+  "https://cdn.kalt.co/citrusstudio.no/diawene/05.jpg",
+  "https://cdn.kalt.co/citrusstudio.no/diawene/12.jpg",
+  "https://cdn.kalt.co/citrusstudio.no/diawene/33.jpg)"
+  "https://cdn.kalt.co/citrusstudio.no/diawene/24.jpg)"
+  "https://cdn.kalt.co/citrusstudio.no/diawene/52.jpg)"
 ]
 
 function shuffle(array) {
