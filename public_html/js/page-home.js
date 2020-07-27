4363feb64ca1054d2bcff5d5eabd9bb871634527
christerwaters <42318403+christerwaters@users.noreply.github.com>
2020-07-27 20:21:57 +0200

    pushed

--- public_html/js/page-home.js
+++ public_html/js/page-home.js
@@ -1,3 +1,21 @@
 $('#projects nav').randomize('#projects nav a');
 $('article').randomize('figure');
 $('article').randomize('figure');
+
+const flipper = [
+  "https://cdn.kalt.co/citrusstudio.no/sykle-uten-hender/01.jpg",
+  "https://cdn.kalt.co/citrusstudio.no/sykle-uten-hender/02.jpg",
+  "https://cdn.kalt.co/citrusstudio.no/sykle-uten-hender/03.jpg",
+  "https://cdn.kalt.co/citrusstudio.no/sykle-uten-hender/04.jpg",
+  "https://cdn.kalt.co/citrusstudio.no/sykle-uten-hender/05.jpg",
+  "https://cdn.kalt.co/citrusstudio.no/for-the-bright-days/01.jpg",
+  "https://cdn.kalt.co/citrusstudio.no/for-the-bright-days/02.jpg",
+  "https://cdn.kalt.co/citrusstudio.no/for-the-bright-days/03.jpg",
+  "https://cdn.kalt.co/citrusstudio.no/for-the-bright-days/04.jpg",
+  "https://cdn.kalt.co/citrusstudio.no/for-the-bright-days/05.jpg",
+  "https://cdn.kalt.co/citrusstudio.no/diawene/05.jpg",
+  "https://cdn.kalt.co/citrusstudio.no/diawene/12.jpg",
+  "https://cdn.kalt.co/citrusstudio.no/diawene/33.jpg)"
+  "https://cdn.kalt.co/citrusstudio.no/diawene/24.jpg)"
+  "https://cdn.kalt.co/citrusstudio.no/diawene/52.jpg)"
+]
--- public_html/js/partial-flipper.js
+++ public_html/js/partial-flipper.js
@@ -1,20 +1,4 @@
-const flipper = [
-  "https://cdn.kalt.co/citrusstudio.no/sykle-uten-hender/01.jpg",
-  "https://cdn.kalt.co/citrusstudio.no/sykle-uten-hender/02.jpg",
-  "https://cdn.kalt.co/citrusstudio.no/sykle-uten-hender/03.jpg",
-  "https://cdn.kalt.co/citrusstudio.no/sykle-uten-hender/04.jpg",
-  "https://cdn.kalt.co/citrusstudio.no/sykle-uten-hender/05.jpg",
-  "https://cdn.kalt.co/citrusstudio.no/for-the-bright-days/01.jpg",
-  "https://cdn.kalt.co/citrusstudio.no/for-the-bright-days/02.jpg",
-  "https://cdn.kalt.co/citrusstudio.no/for-the-bright-days/03.jpg",
-  "https://cdn.kalt.co/citrusstudio.no/for-the-bright-days/04.jpg",
-  "https://cdn.kalt.co/citrusstudio.no/for-the-bright-days/05.jpg",
-  "https://cdn.kalt.co/citrusstudio.no/diawene/05.jpg",
-  "https://cdn.kalt.co/citrusstudio.no/diawene/12.jpg",
-  "https://cdn.kalt.co/citrusstudio.no/diawene/33.jpg)"
-  "https://cdn.kalt.co/citrusstudio.no/diawene/24.jpg)"
-  "https://cdn.kalt.co/citrusstudio.no/diawene/52.jpg)"
-]
+
 
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
